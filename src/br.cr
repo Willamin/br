0925283d030aac2b6412cb261014821709719506
Will Lewis <will@wflewis.com>
2018-06-30 17:07:30 -0400

    Modularize

--- src/br.cr
+++ src/br.cr
@@ -1,6 +1,12 @@
 require "file_utils"
 require "tempfile"
 
+class Object
+  def tap_into
+    with self yield
+  end
+end
+
 module Br
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
@@ -8,6 +14,8 @@ module Br
   ARGS    = ARGV - FLAGS
   DRY_RUN = (ARGV & FLAGS).includes?("--dry-run")
   VERBOSE = (ARGV & FLAGS).includes?("--verbose")
+
+  EDITOR = ENV["EDITOR"]? || "vim"
 
   class RenameAction
     property from : String?
@@ -33,42 +41,61 @@ module Br
     end
   end
 
-  def self.add_star(globs)
-    globs.map do |dir|
-      if Dir.exists?(dir)
-        File.join(dir, "*")
-      else
-        dir
+  class Cli
+    @globs = [] of String
+    @renames = [] of Br::RenameAction
+    @temp : Tempfile
+
+    def initialize
+      @globs = Br::ARGS
+      @temp = Tempfile.new("br")
+    end
+
+    def add_star
+      @globs = @globs.map do |dir|
+        if Dir.exists?(dir)
+          File.join(dir, "*")
+        else
+          dir
+        end
+      end
+    end
+
+    def write_to_temp
+      file = File.open(@temp.path, mode: "a")
+
+      Dir.glob(@globs) do |dir|
+        file.puts(dir)
+        ra = Br::RenameAction.new
+        ra.from = dir
+        @renames << ra
+      end
+
+      file.close
+    end
+
+    def show_editor
+      `#{EDITOR} #{@temp.path}`
+    end
+
+    def read_from_temp
+      File.read_lines(@temp.path).each_with_index do |line, index|
+        @renames[index].to = line
+      end
+    end
+
+    def rename!
+      @renames.each do |ra|
+        ra.rename!
       end
     end
   end
 end
 
-globs = Br::ARGS
-globs = Br.add_star(globs)
-
-renames = [] of Br::RenameAction
-
-temp = Tempfile.new("br")
-file = File.open(temp.path, mode: "a")
-
-Dir.glob(globs) do |dir|
-  file.puts(dir)
-  ra = Br::RenameAction.new
-  ra.from = dir
-  renames << ra
-end
-
-file.close
-
-command = ENV["EDITOR"]? || "vim"
-
-`#{command} #{temp.path}`
-
-File.read_lines(temp.path).each_with_index do |line, index|
-  renames[index].to = line
-end
-
-renames.each do |ra|
-  ra.rename!
+Br::Cli.new.tap_into do
+  add_star
+  write_to_temp
+  show_editor
+  read_from_temp
+  rename!
 end
