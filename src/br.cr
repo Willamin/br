001842d2f9340535c3dcf1c79297d7317117ddee
Will Lewis <will@wflewis.com>
2018-06-30 01:51:45 -0400

    Add first iteration of the program

--- src/br.cr
+++ src/br.cr
@@ -1,3 +1,57 @@
+require "file_utils"
+require "tempfile"
+
 module Br
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+  VERBOSE = false
+
+  class RenameAction
+    property from : String?
+    property to : String?
+
+    def rename!
+      STDERR.puts "#{from} -> #{to}" if VERBOSE
+      from.try do |from|
+        to.try do |to|
+          FileUtils.mv(from, to)
+        end
+      end
+    end
+  end
+end
+
+globs = ARGV
+
+globs = globs.map do |dir|
+  if Dir.exists?(dir)
+    File.join(dir, "*")
+  else
+    dir
+  end
+end
+
+renames = [] of Br::RenameAction
+
+temp = Tempfile.new("br")
+file = File.open(temp.path, mode: "a")
+
+Dir.glob(globs) do |dir|
+  file.puts(dir)
+  ra = Br::RenameAction.new
+  ra.from = dir
+  renames << ra
+end
+
+file.close
+
+command = ENV["EDITOR"]? || "vim"
+
+`#{command} #{temp.path}`
+
+File.read_lines(temp.path).each_with_index do |line, index|
+  renames[index].to = line
+end
+
+renames.each do |ra|
+  ra.rename!
 end
