f5d9a6221392f7b8e5e0115d583594f2fcae3f8f
Will Lewis <will@wflewis.com>
2018-07-02 10:45:39 -0400

    Add tests

--- spec/br_spec.cr
+++ spec/br_spec.cr
@@ -1,9 +1,43 @@
 require "./spec_helper"
+require "file_utils"
+`shards build --no-debug`
+
+def setup
+  t = Tempfile.dirname
+  spec_dir = File.join(t, "br_spec")
+  FileUtils.rm_r(spec_dir)
+  FileUtils.mkdir(spec_dir)
+  %w[foo bar baz].each do |name|
+    f = File.open(File.join(spec_dir, name), mode: "w")
+    f.puts("originally #{name}")
+    f.close
+  end
+
+  spec_dir
+end
+
+def br
+  "EDITOR=#{__DIR__}/tac bin/br"
+end
 
 describe Br do
-  # TODO: Write tests
+  test "that files are renamed" do
+    dir = setup
 
-  test "that it works" do
-    assert 0 == 1
+    `#{br} #{dir}`
+
+    %w[foo bar baz].each do |name|
+      assert File.read("#{dir}/#{name}-updated") == "originally #{name}\n"
+    end
+  end
+
+  test "that version is returned" do
+    output = `#{br} --version`.strip
+    assert output == Br::VERSION
+  end
+
+  test "that that usage is shown when no args are given" do
+    output = `#{br}`.strip
+    assert output == Br::USAGE.strip
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,4 @@
-require "../src/br"
+require "../src/br/*"
 require "microtest"
 include Microtest::DSL
 Microtest.run!
